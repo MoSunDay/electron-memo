6df8c9a0f5334b9d3a30aade64f9d3405b2b581e
Mo_s <heyang@dayuwuxian.com>
2021-06-09 12:08:17 +0800

    ADD: auto sort

--- src/components/TodoList/reducer.tsx
+++ src/components/TodoList/reducer.tsx
@@ -1,5 +1,6 @@
 import { ACTION_TYPE, IAction } from "./typings";
 import { IState, ITodo } from "./typings";
+import arrayMove from "array-move";
 
 function todoReducer(state: IState, action: IAction): IState {
   const { type, payload } = action;
@@ -8,7 +9,7 @@ function todoReducer(state: IState, action: IAction): IState {
     case ACTION_TYPE.ADD_TODO:
       return {
         ...state,
-        todoList: [...state.todoList, payload as ITodo],
+        todoList: [payload as ITodo, ...state.todoList],
       };
     case ACTION_TYPE.REMOVE_TODO:
       return {
@@ -16,18 +17,38 @@ function todoReducer(state: IState, action: IAction): IState {
         todoList: state.todoList.filter((todo: ITodo) => todo.id !== payload),
       };
     case ACTION_TYPE.TOGGLE_TODO:
+      let stateTodoList = state.todoList
+      let completedIndex = stateTodoList.length - 1
+      let todoIndex = -1
+      let toggleDone = true;
+      let i: number
+
+      for (i = 0; i < stateTodoList.length; i++) {
+        if (todoIndex !== -1 && completedIndex !== stateTodoList.length - 1) {
+          break
+        }
+        if (stateTodoList[i].id === payload) {
+          todoIndex = i
+          stateTodoList[i].completed = !stateTodoList[i].completed
+          toggleDone = stateTodoList[i].completed
+          continue
+        }
+        if (stateTodoList[i].completed === true && completedIndex === stateTodoList.length - 1) {
+          completedIndex = i
+        }
+      }
+      if (todoIndex !== -1) {
+        if (toggleDone) {
+          completedIndex = completedIndex === stateTodoList.length - 1 ? completedIndex : completedIndex - 1
+          stateTodoList = arrayMove(stateTodoList, todoIndex, completedIndex)
+        } else {
+          completedIndex = todoIndex < completedIndex ? completedIndex - 1 : completedIndex
+          stateTodoList = arrayMove(stateTodoList, todoIndex, completedIndex)
+        }
+      }
       return {
         ...state,
-        todoList: state.todoList.map((todo: ITodo) => {
-          return todo.id === payload
-            ? {
-                ...todo,
-                completed: !todo.completed,
-              }
-            : {
-                ...todo,
-              };
-        }),
+        todoList: stateTodoList,
       };
     case ACTION_TYPE.INIT_TODOLIST:
       return {
